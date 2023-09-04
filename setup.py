cce50a5bc086a00c8d1abea3fb50992a384786db
Khmelev Nikita Anatolevich <khmelev@speechpro.com>
2023-09-05 02:32:13 +0300

    quick start

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='nirma',
+      version='0.0.1',
+      description='Nirma',
+      packages=find_packages(include=['src', 'src.*'])
+     )
--- src/pipelines/test_pipeline.py
+++ src/pipelines/test_pipeline.py
@@ -1,6 +1,7 @@
 from src.base.pipeline_constructor import BasePipeline
 import logging
 import sys
+import json
 
 
 logger = logging.getLogger()
@@ -14,6 +15,8 @@ class TestPipeline(BasePipeline):
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    cfg_path = "pipeline_cofigs/test_pipeline.yaml"
+    cfg_path = "src/pipelines/pipeline_cofigs/test_pipeline.yaml"
     pipeline = TestPipeline.init_from_config(cfg_path)
-    pipeline.process({})
+    res = pipeline.process({"audio": "Path to wav"})
+    with open("test.json", "w") as f:
+        json.dump(res["segments"], fp=f, indent=4, ensure_ascii=False)
