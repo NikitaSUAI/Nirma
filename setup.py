e6d4ba6a2d9be11bffc93ab2fcb84d7ed62820b8
Khmelev Nikita Anatolevich <khmelev@speechpro.com>
2023-09-05 13:25:58 +0300

    requirements

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='nirma',
-      version='0.0.1',
-      description='Nirma',
-      packages=find_packages(include=['src', 'src.*'])
-     )
