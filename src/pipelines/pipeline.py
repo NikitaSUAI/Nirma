44f822584655df5f1c8096060036e3f9ea128c81
Khmelev Nikita Anatolevich <khmelev@speechpro.com>
2023-09-05 15:47:43 +0300

    fix req

--- src/pipelines/pipeline.py
+++ src/pipelines/pipeline.py
@@ -2,7 +2,7 @@ from src.base.pipeline_constructor import BasePipeline
 import logging
 import sys
 import json
-import click
+import argparse
 
 logger = logging.getLogger()
 
@@ -11,20 +11,24 @@ class TestPipeline(BasePipeline):
 
     def __init__(self, cfg):
         super().__init__(cfg)
-@click.command()
-@click.option("--wav_file", "-w")
-@click.option("--output", "-o", default="test.json")
-@click.option("--conf", "-c", default="src/pipelines/pipeline_cofigs/test_pipeline.yaml")
-def main(wav_file, output, conf):
+
+
+def main():
+    parser = argparse.ArgumentParser(
+                    prog='Nirma pipeline')
+    parser.add_argument('-w', '--wav_file')
+    parser.add_argument('-o', '--output', default="test.json")
+    parser.add_argument('-c', '--cfg_path', default="src/pipelines/pipeline_cofigs/test_pipeline.yaml")
+    args = parser.parse_args()
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    cfg_path = conf
-    pipeline = TestPipeline.init_from_config(cfg_path)
-    res = pipeline.process({"audio": wav_file})
+    
+    pipeline = TestPipeline.init_from_config(args.cfg_path)
+    res = pipeline.process({"audio": args.wav_file})
     for idx, (emo, vd) in enumerate(zip(res["segments_emotions"], res["segments_valence_dominance"])):
         res["segments"][idx]['emotion'] = emo
         res["segments"][idx]['valence'] = str(vd[0])
         res["segments"][idx]['dominance'] = str(vd[1])
-    with open(output, "w") as f:
+    with open(args.output, "w") as f:
         json.dump(res["segments"], fp=f, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
